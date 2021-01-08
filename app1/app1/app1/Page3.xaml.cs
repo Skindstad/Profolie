b83f7589eb62fdbd20a1bf2cb5fd25ed9109b823
Skindstad <39044695+Skindstad@users.noreply.github.com>
2021-01-08 08:11:39 +0100

    App

---
+++ app1/app1/app1/Page3.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace app1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page3 : ContentPage
+    {
+        public Page3()
+        {
+            InitializeComponent();
+
+
+            ScrollView scrollView = new ScrollView();
+            scrollView.Scrolled += ScrollView_Scrolled;
+        }
+        
+
+        private void ScrollView_Scrolled(object sender, ScrolledEventArgs e)
+        {
+            Console.WriteLine($"ScrollX: {e.ScrollX}, ScrollY: {e.ScrollY}");
+        }
+    }
+}
---
+++ app1/app1/app1/Page4.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace app1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page4 : ContentPage
+    {
+        public Page4()
+        {
+            InitializeComponent();
+
+            ScrollView scrollView = new ScrollView();
+            scrollView.Scrolled += ScrollView_Scrolled;
+        }
+
+        private void ScrollView_Scrolled(object sender, ScrolledEventArgs e)
+        {
+            Console.WriteLine($"ScrollX: {e.ScrollX}, ScrollY: {e.ScrollY}");
+        }
+    }
+}
